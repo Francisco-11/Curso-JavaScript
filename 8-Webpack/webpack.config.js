4376bb0c7e34b63127d71a8a9c6ec0c900a59c4b
Francisco-11 <informatica.vera2@gmail.com>
2020-05-26 20:26:01 -0400

    Seccion de webpack y sus configuraciones, hasta el video 89

---
+++ 8-Webpack/webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssestsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+
+    mode: 'development',
+    optimization:{
+        minimizer: [
+            new OptimizeCssAssestsPlugin()
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ] 
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize:false
+                },
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        })
+    ]
+
+
+
+}
+
+
+
+
