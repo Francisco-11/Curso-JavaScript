cb91e895b3f8236914d8799b154aa7e822e3c579
Francisco-11 <informatica.vera2@gmail.com>
2020-05-28 19:02:00 -0400

    Inicio de la seccion 12, y realizacion de peticiones HTTP con Fetch

---
+++ 12-http/src/index.js
@@ -0,0 +1,24 @@
+
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+
+fetch( jokeUrl ).then( resp => {
+    // Forma 1:
+    // resp.json().then(data => {
+        // console.log(data.id);
+        // console.log(data.value);
+    // });
+
+    // Forma 2:
+    resp.json().then(({id,value}) => {
+        console.log(id);
+        console.log(value);
+    })
+});
+
+
+
+
+
+
+
+
