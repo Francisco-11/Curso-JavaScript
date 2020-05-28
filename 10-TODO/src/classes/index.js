62bcc9ff849b13ffdf72423c519b9382788ff442
Francisco-11 <informatica.vera2@gmail.com>
2020-05-28 09:31:53 -0400

    Se termina seccion 1o lista para github pages

---
+++ 10-TODO/src/classes/index.js
@@ -0,0 +1,10 @@
+// Servira para exportar todas las clases y centralizarlas.
+
+import { Todo } from './todo.class';
+import { TodoList } from './todo-list.class';
+
+
+export {
+    Todo,
+    TodoList
+}
--- 10-TODO/src/index.js
+++ 10-TODO/src/index.js
@@ -1,9 +1,36 @@
 
-import {saludar} from './js/componentes';
 import './styles.css';
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
-const nombre = 'Francisco';
+export const todoList = new TodoList();
+/* const tarea = new Todo('Aprender JavaScript!!');
 
-saludar(nombre);
+todoList.nuevoTodo(tarea);
 
+
+console.log(todoList);
+
+crearTodoHtml(tarea);
+
+
+localStorage.setItem('mi-key','ABCD123');
+sessionStorage.setItem('mi-key','ABCD123');
+ */
+/* setTimeout( () => {
+    localStorage.removeItem('mi-key')
+}, 1500);
+
+ */
+// Los tengo todos, falta reconstruirlos en el HTML
+//  console.log(todoList.todos);
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+
+const newTodo = new Todo('Aprender JavaScript');
+// todoList.nuevoTodo(newTodo);
+todoList.todos[0].imprimirClase();
+//newTodo.imprimirClase();
+
+console.log('todo ', todoList.todos);
