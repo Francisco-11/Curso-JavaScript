27740b40b891432c2d9b0a9afdb5c53bcc513b4f
Francisco-11 <informatica.vera2@gmail.com>
2020-05-28 18:34:03 -0400

    Termino de la seccion 11, con la explicacion del for await e If await

--- 11-Promesas-Callbacks/src/index.js
+++ 11-Promesas-Callbacks/src/index.js
@@ -1,10 +1,27 @@
+// Para IFAWAIT
 
-import { obtenerHeroesArr } from './js/await'
+import {heroesCiclo, heroeIfAwait} from './js/await';
 
-console.time('await');
+heroesCiclo();
 
-obtenerHeroesArr().then( heroes => {
-    console.log(heroes);
-    console.timeEnd('await');
-});
+
+heroeIfAwait('iron');
+/* 
+import { obtenerHeroesArr, obtenerHeroeAwait } from './js/await'
+
+console.time('await'); */
+
+// obtenerHeroesArr().then( heroes => {
+    // console.log(heroes);
+    // console.timeEnd('await');
+// });
+
+/* obtenerHeroeAwait('capi')
+    .then( heroe => {
+        console.log(heroe);
+        console.timeEnd('await');
+    }).catch(console.warn);
+
+ */
+
 
--- 11-Promesas-Callbacks/src/js/await.js
+++ 11-Promesas-Callbacks/src/js/await.js
@@ -6,6 +6,8 @@ const heroesIds = [
     'iron',
     'spider'
 ];
+const heroesPromesas = heroesIds.map( id => buscarHeroe(id));
+
 
 export const obtenerHeroesArr = async() => {
 
@@ -39,4 +41,43 @@ export const obtenerHeroesArr = async() => {
     // Forma 4
 
     return await Promise.all(heroesIds.map(buscarHeroe));
+}
+
+export const obtenerHeroeAwait = async (id) => {
+                // Aqui esoy obteniendo una promesa
+    try { //Intenta hacer este codigo
+        const heroe = await buscarHeroeAsync(id);
+        return heroe;
+        
+    }catch(err){
+        console.log('CATCH!!!');
+        return {
+            nombre: 'Sin nombre',
+            poder: 'Sin poder' 
+        };
+    }
+    
+}
+
+export const heroesCiclo = async () => {
+    console.time('HeroesCiclo');
+
+    // Otra forma
+    for await(const heroe of heroesPromesas){
+        console.log(heroe);
+    }
+    
+    // Una forma
+    // const heroes = await Promise.all(heroesPromesas);
+    // heroes.forEach(heroe => console.log(heroe));
+    // console.log(heroes);
+    console.timeEnd('HeroesCiclo');
+}
+
+export const heroeIfAwait = async(id) => {
+    if( (await buscarHeroeAsync(id)).nombre==='Ironman'){
+        console.log('Es el mejor de todos');
+    }else {
+        console.log('Naaaa');
+    }
 }
